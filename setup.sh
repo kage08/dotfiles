ce837e65e2f4b94785933ad0df30b4b11bfd7228
kage08 <harshavardhan864.hk@gmail.com>
2022-06-23 18:03:13 +0000

    Commands to stramline adding to PATH

--- setup.sh
+++ setup.sh
@@ -5,14 +5,21 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh-bin/master/insta
 wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.tar.gz
 tar xvf nvim-linux64.tar.gz
 ln -s $HOME/nvim-linux64/bin/nvim ~/.local/bin/nvim
+rm nvim-linux64.tar.gz
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Add ~/.local/bin to PATH
+echo -e '\nexport PATH="/home/user/.local/bin:$PATH"\n' >> ~/.bashrc
+source ~/.bashrc
 
 # Install OMZ
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh
 curl -sS https://starship.rs/install.sh ~/.local/bin | sh -s -- --bin-dir ~/.local/bin
+echo -e '\nexport PATH="/home/user/.local/bin:$PATH"\n' >> ~/.zshrc
+echo -e 'eval "$(starship init zsh)"\n' >> ~/.zshrc
+source ~/.zshrc
 
 git clone https://github.com/kage08/dotfiles.git
 cd dotfiles
@@ -22,9 +29,11 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # Auto-suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # Sub-string search
- git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
- # fzf
- git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 ./pull.sh
+source ~/.zshrc
+cd
