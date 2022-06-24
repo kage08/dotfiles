f4f8dc31209fb22057c57f810c2ef5de7e59efee
Harshavardhan Kamarthi <harshavardhan864.hk@gmail.com>
2022-06-24 10:23:20 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -65,3 +65,4 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
 conda install mamba -c conda-forge
+mamba clean --all
