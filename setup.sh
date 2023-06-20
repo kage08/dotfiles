fe2d9e3eb09ad891617dce55c24924ed616f6f75
Harshavardhan Kamarthi <harshavardhan864.hk@gmail.com>
2023-06-20 03:01:01 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -76,8 +76,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -fsSL https://install.julialang.org | sh
 
 # Rust utils
-cargo install jql exa bat du-dust ripgrep tokei hyperfine ytop zoxide git-delta bottom sd dutree
-cargo install bob-nvim gitui irust evcxr_jupyter cargo-info nu zellij mprocs coreutils cargo-update
+cargo install jql exa bat du-dust ripgrep tokei hyperfine ytop zoxide git-delta bottom sd dutree flamegraph samply
+cargo install bob-nvim gitui irust evcxr_jupyter cargo-info nu zellij mprocs coreutils cargo-update counts
 
 # latex install
 cd
