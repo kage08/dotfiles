988d858b114e04857ee21284d373dfb2ae571881
Harshavardhan Kamarthi <harshavardhan864.hk@gmail.com>
2023-12-09 20:07:55 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh-bin/master/insta
 #Neovim
 wget https://github.com/neovim/neovim/releases/download/v0.9.4/nvim-linux64.tar.gz
 tar xvf nvim-linux64.tar.gz
-ln -s $HOME/nvim-linux64/bin/nvim ~/.local/bin/nvim
+ln -s $HOME/nvim-linux64/bin/nvim ~/.local/bin
 rm nvim-linux64.tar.gz
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
